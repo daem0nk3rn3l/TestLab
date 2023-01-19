abf9fd1c6219004526fee1850cfe8e5990066e4b
Linda <118927607+daem0nk3rn3l@users.noreply.github.com>
2023-01-19 00:12:49 -0500

    Update ComputerHDStats.ps1

--- ComputerHDStats.ps1
+++ ComputerHDStats.ps1
@@ -8,3 +8,6 @@ Select-Object DeviceID,`
 @{Name="SizeMB";Expression={$_.size/1MB -as [int]}},`
 @{Name="FreeMB";Expression={$_.freespace/1mb -as [int]}},`
 @{Name="PctFree";Expression={[math]::round(($_.freespace/$_.size)*100,2)}}
+
+
+
