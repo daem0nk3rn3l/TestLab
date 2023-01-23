4c9e6a26807ed55e7695fa1b645eb27a33903c9f
Linda <118927607+daem0nk3rn3l@users.noreply.github.com>
2023-01-23 18:35:38 -0500

    Update WiFi_Info.ps1

--- WiFi_Info.ps1
+++ WiFi_Info.ps1
@@ -1,7 +1,16 @@
 #  Get Wi-Fi Name / Password
-#  https://www.elevenforum.com/t/find-wi-fi-network-security-key-password-in-windows-11.4475/
+#
+#
 
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} `
 | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" `
 | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table `
 | Out-File "$env:userprofile\OneDrive\Desktop\Wi-Fi_network_passwords.txt"
+
+
+
+
+
+
+
+
