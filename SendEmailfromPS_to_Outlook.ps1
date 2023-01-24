d01b658b5156c1e180a971b07bd14a60d49992c9
daem0nk <zelle_accts@outlook.com>
2023-01-23 20:35:38 -0500

    Send email via PowerShell

---
+++ SendEmailfromPS_to_Outlook.ps1
@@ -0,0 +1,32 @@
+﻿
+# create an object 
+$OL = New-Object -ComObject outlook.application
+
+
+Start-Sleep 5
+
+<#
+olAppointmentItem
+olContactItem
+olDistributionListItem
+olJournalItem
+olMailItem
+olNoteItem
+olPostItem
+olTaskItem
+#>
+
+
+
+# Create Item
+$mItem = $OL.CreateItem("olMailItem")
+
+
+$mItem.To = "xyz000@gmail.com"
+$mItem.Subject = "PowerMail"
+$mItem.Body = "Sent from PowerShell"
+
+$mItem.Send()
+
+
+
