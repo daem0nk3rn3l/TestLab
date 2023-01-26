1608aa7ebd3881d34f1f4bf5ab0ea367e6cff9d1
Linda <118927607+daem0nk3rn3l@users.noreply.github.com>
2023-01-25 21:46:32 -0500

    Create RandomPWGenerator.py
    Random password generator

---
+++ RandomPWGenerator.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    # Create a set of characters to use in the password
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()_+-=[]{};:,.<>/?|"
+
+    # create a random password
+    password = "".join(random.choice(characters) for i in range(length))
+
+    # Output the password to the command line
+
+    print("Password: " + password)
+
+if __name__ == "__main__":
+
+    # Generate a password with a random length between 12 and 16 characters
+
+    length = random.randint(12, 16)
+
+    print("MakePass Random Password Genreator v1.0")
+    print("")
+    generate_password(length)
+    
+    
